3e963573ac46afc7c28f14456c67606223636771
Jefferson Rodríguez <jefferson.rc94@gmail.com>
2025-08-27 19:19:34 +0200

    finishing MBRS ..

--- modules/watermarking/MBRS/utils/load_train_setting.py
+++ modules/watermarking/MBRS/utils/load_train_setting.py
@@ -21,36 +21,44 @@ train_continue_path = settings.train_continue_path
 train_continue_epoch = settings.train_continue_epoch
 save_images_number = settings.save_images_number
 lr = settings.lr
+SAVE_EVERY_N_STEPS = settings.SAVE_EVERY_N_STEPS
+VALIDATE_EVERY_N_STEPS = settings.VALIDATE_EVERY_N_STEPS
+train_continue_step = settings.train_continue_step
 H, W, message_length = settings.H, settings.W, settings.message_length,
 noise_layers = settings.noise_layers
+experiment_name = settings.experiment_name
 
 '''
 file preparing
 '''
-full_project_name = project_name + '_' + str(H) +"_m" + str(message_length)
-#for noise in noise_layers:
-#	full_project_name += "_" + noise
+if train_continue:
+	full_project_name = experiment_name
+	result_folder = "runs/" + full_project_name + "/" + dataset_train + "/"
+else:
+	full_project_name = project_name + '_' + str(H) +"_m" + str(message_length)
+	#for noise in noise_layers:
+	#	full_project_name += "_" + noise
 
-result_folder = "runs/" + time.strftime(full_project_name + "__%Y_%m_%d__%H_%M_%S", time.localtime()) + "/" + dataset_train + "/"
-if not os.path.exists(result_folder): os.makedirs(result_folder)
-if not os.path.exists(result_folder + "samples/"): os.makedirs(result_folder + "samples/")
-if not os.path.exists(result_folder + "model/"): os.makedirs(result_folder + "model/")
+	result_folder = "runs/" + time.strftime(full_project_name + "__%Y_%m_%d__%H_%M_%S", time.localtime()) + "/" + dataset_train + "/"
+	if not os.path.exists(result_folder): os.makedirs(result_folder)
+	if not os.path.exists(result_folder + "samples/"): os.makedirs(result_folder + "samples/")
+	if not os.path.exists(result_folder + "model/"): os.makedirs(result_folder + "model/")
 
-with open(result_folder + "/train_params.txt", "w") as file:
-	content = "-----------------------" + time.strftime("Date: %Y/%m/%d %H:%M:%S",
-														time.localtime()) + "-----------------------\n"
+	with open(result_folder + "/train_params.txt", "w") as file:
+		content = "-----------------------" + time.strftime("Date: %Y/%m/%d %H:%M:%S",
+															time.localtime()) + "-----------------------\n"
 
-	for item in settings.get_items():
-		content += item[0] + " = " + str(item[1]) + "\n"
+		for item in settings.get_items():
+			content += item[0] + " = " + str(item[1]) + "\n"
 
-	print(content)
-	file.write(content)
+		print(content)
+		file.write(content)
 
-with open(result_folder + "/train_log.txt", "w") as file:
-	content = "-----------------------" + time.strftime("Date: %Y/%m/%d %H:%M:%S",
-														time.localtime()) + "-----------------------\n"
-	file.write(content)
-with open(result_folder + "/val_log.txt", "w") as file:
-	content = "-----------------------" + time.strftime("Date: %Y/%m/%d %H:%M:%S",
-														time.localtime()) + "-----------------------\n"
-	file.write(content)
+	with open(result_folder + "/train_log.txt", "w") as file:
+		content = "-----------------------" + time.strftime("Date: %Y/%m/%d %H:%M:%S",
+															time.localtime()) + "-----------------------\n"
+		file.write(content)
+	with open(result_folder + "/val_log.txt", "w") as file:
+		content = "-----------------------" + time.strftime("Date: %Y/%m/%d %H:%M:%S",
+															time.localtime()) + "-----------------------\n"
+		file.write(content)
